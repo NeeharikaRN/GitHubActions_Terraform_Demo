3f9a00afb6aebda3c4d2ca2910053c5577d52830
NeeharikaRN <n.neeharikareddy@gmail.com>
2024-12-04 09:34:53 +0530

    Update root.tf

--- terraform/root.tf
+++ terraform/root.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
 
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {
     bucket         = "neeha-s3bucket-date"
     key            = "globalstate/s3/terraform.tfstate"
-    region         = "eu-west-1"
+    region         = "us-east-1"
     dynamodb_table = "neeha-date-dblocks"
     encrypt        = true
   }
